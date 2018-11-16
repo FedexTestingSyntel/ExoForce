a6ecf84d84d9f56cb6f0788e86d6e6659d13200c
Sean <42709217+FedexTestingSyntel@users.noreply.github.com>
2018-11-16 12:27:48 -0600

    Working on using data class for the wFCL scnerios

--- ExoForce/src/main/java/USRC/USRC_FDM.java
+++ ExoForce/src/main/java/USRC/USRC_FDM.java
@@ -22,7 +22,7 @@ import Data_Structures.*;
 
 public class USRC_FDM {
 
-	static String LevelsToTest = "2"; //Can but updated to test multiple levels at once if needed. Setting to "23" will test both level 2 and level 3.
+	static String LevelsToTest = "3"; //Can but updated to test multiple levels at once if needed. Setting to "23" will test both level 2 and level 3.
 
 	@BeforeClass
 	public void beforeClass() {
